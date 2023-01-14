4fdc3c3f1019827ae3fe8d88d9cb75583f5b10fb
Matheus Teixeira <matheustxr.dev@gmail.com>
2023-01-14 13:32:04 -0300

    header finished and main1 started

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,22 @@
 import './Home.css'
 
+import {Header} from './header/Header'
+import {Main1} from './main/index'
+
+
 function Home() {
   return (
     <div className="Home">
+      <div className='background-image'>
+        {
+          <Header/>
+        }
+
+        {
+          <Main1/>
+        }
+        
+      </div>
       
     </div>
   )
---
+++ src/pages/home/main/index.tsx
@@ -0,0 +1,9 @@
+import "./style.css"
+
+export function Main1() {
+    return (
+        <div className="main1">
+            
+        </div>
+    )
+}
