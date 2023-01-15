13b6e56ec94e06f32722ae6385403688e4d72c3d
Matheus Teixeira <matheustxr.dev@gmail.com>
2023-01-15 10:53:40 -0300

    galery fineshed and main2 inizilized

--- src/pages/home/components/galery/index.tsx
+++ src/pages/home/components/galery/index.tsx
@@ -2,6 +2,22 @@ import './style.css';
 
 export function Galery() {
     return (
-        <div className="galery"></div>
+        <div className="galery">
+            
+            <span id="galery1">
+                <img src="src\assets\galery\galeria-1.png" alt="imagem do astronauta 1" />
+                <img src="src\assets\galery\galeria-2.png" alt="imagem do astronauta 2" />
+                <img src="src\assets\galery\galeria-3.png" alt="imagem do astronauta 3" />
+                <img src="src\assets\galery\galeria-4.png" alt="imagem do astronauta 4" />
+            </span>
+            
+            <span id="galery2">
+                <img src="src\assets\galery\galeria-5.png" alt="imagem do astronauta 5" />
+                <img src="src\assets\galery\galeria-6.png" alt="imagem do astronauta 6" />
+                <img src="src\assets\galery\galeria-7.png" alt="imagem do astronauta 7" />
+                <img src="src\assets\galery\galeria-8.png" alt="imagem do astronauta 8" />
+            </span>
+            
+        </div>
     )
 }
